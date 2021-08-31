7b39e3825bb13e5cfac3aca588a07a14621f4d04
Paulo Elienay <paulo@pauloelienay.com>
2021-08-31 10:17:49 -0300

    feat: implements `Header`

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+	items: MenuItem[];
 
-  constructor() { }
+	constructor() {
+		this.items = [
+			{ label: 'Imageboard', url: '/' },
+			{
+				label: 'Posts',
+				url: '/posts',
+			},
+		];
+	}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-
-
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+	declarations: [HeaderComponent],
+	imports: [CommonModule, MenubarModule],
+	exports: [HeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
